cc720f176273ec30611828ef7509bd854b44e641
Michiko18 <47420317+Michiko18@users.noreply.github.com>
2020-04-28 15:51:39 +0800

    Update ResetPassword.cs

--- Assignment - Design/ResetPassword.cs
+++ Assignment - Design/ResetPassword.cs
@@ -15,19 +15,6 @@ namespace Assignment___Design
         public ResetPassword()
         {
             InitializeComponent();
-	    panel1.Visible = true;
-        }
-	private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-	private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
-	private void textBox4_TextChanged(object sender, EventArgs e)
-        {
-
         }
     }
 }
