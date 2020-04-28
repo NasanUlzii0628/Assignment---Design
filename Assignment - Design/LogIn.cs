9ae70c894190e2663d736b3ae14c6b9145d76c47
Michiko18 <47420317+Michiko18@users.noreply.github.com>
2020-04-28 15:38:46 +0800

    Create LogIn.cs

---
+++ Assignment - Design/LogIn.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BieDaalt
+{
+    public partial class LogIn : Form
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Home df = new Home();
+            df.Show();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            ResetPassword f3 = new ResetPassword();
+            f3.Show();
+        }
+    }
+}
